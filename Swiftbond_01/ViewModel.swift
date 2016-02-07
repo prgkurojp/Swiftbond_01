a3c74ce5c6ab10190ba215502ffd4ef3f349b6de
Toshifumi Kurosawa <kuromailserver@gmail.com>
2016-02-08 00:54:42 +0900

    first commit

---
+++ Swiftbond_01/ViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModel.swift
+//  Swiftbond_01
+//
+//  Created by 黒澤俊文 on 2016/02/08.
+//  Copyright © 2016年 黒澤俊文. All rights reserved.
+//
+
+import UIKit
+
+class ViewModel: NSObject {
+
+}

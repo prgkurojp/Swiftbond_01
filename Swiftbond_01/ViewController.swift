cebe47b477bf0f894f4d5863e97a2e29b01ffdea
Toshifumi Kurosawa <kuromailserver@gmail.com>
2016-02-08 00:35:38 +0900

    Initial Commit

---
+++ Swiftbond_01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swiftbond_01
+//
+//  Created by 黒澤俊文 on 2016/02/08.
+//  Copyright © 2016年 黒澤俊文. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
